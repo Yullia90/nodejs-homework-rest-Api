e851cdf6407a0efac244744436ce7cc7573bb9f1
Yullia90 <y.tsarenko@harwind.com.ua>
2024-01-03 20:31:03 +0200

    add environment
    move DB_HOST to config.js & add config.js to .gitignore get DB_HOST from process.env

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
-const app = require('./app');
+const mongoose = require("mongoose");
+const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://yuliiatsarenko:PfdbpgDtDW3ff5T0@contacts.fgxyvpu.mongodb.net/db-contacts?retryWrites=true&w=majority";
-mongoose.connect(DB_HOST)
+const { DB_HOST, PORT = 3000 } = process.env;
+
+// Інший код, де використовується DB_HOST
+
+mongoose
+  .connect(DB_HOST)
   .then(() => {
-  app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
   })
-  })
-  .catch(error => {
+  .catch((error) => {
     console.log(error.message);
     process.exit(1);
-})
-
-
-//PfdbpgDtDW3ff5T0;
+  });
