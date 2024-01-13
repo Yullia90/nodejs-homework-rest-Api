02bdcc3f0a5c8f4f9f36dc91f0819514485c2b0a
Yullia90 <y.tsarenko@harwind.com.ua>
2024-01-13 16:08:16 +0200

    add some details

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -3,21 +3,22 @@ const { Contact } = require("../../models/contact");
 const getAllContacts = async (req, res) => {
   const { _id: owner } = req.user;
   const { page = 1, limit = 20, favorite } = req.query;
-
   const skip = (page - 1) * limit;
 
-  let result = await Contact.find({ owner }, "-createdAt -updatedAt", {
-    skip,
-    limit,
-  }).populate("owner", "email subscription");
+  let result;
 
   if (favorite) {
-    result = await Contact.find({ owner, favorite }, "-createdAt -updatedAt", {
+     result = await Contact.find({ owner, favorite }, "-createdAt -updatedAt", {
       skip,
       limit,
       favorite,
     }).populate("owner", "email subscription");
-  }
+  } else {
+    result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "email subscription");}
+  
 
   res.status(200).json(result);
 };
