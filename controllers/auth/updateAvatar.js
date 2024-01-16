d43d56c833075c4735eab0959534ee1a97ad1aa4
Yullia90 <y.tsarenko@harwind.com.ua>
2024-01-16 23:18:50 +0200

    fix temp

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -5,26 +5,50 @@ const { User } = require("../../models/user");
 // const { cloudinary } = require("../../helpers/index");
 
 const avatarDir = path.join(__dirname, "../../", "public", "avatars");
+ 
 
 // Зберігаємо файли в папці
+
 const updateAvatar = async (req, res) => {
-    const { _id } = req.user;
-    const { path: tempUpload, originalname } = req.file;
-    const filename = `${_id}_${originalname}`;
+  const { path: tmpUpload, originalname } = req.file;
 
-    const resultUpload = path.join(avatarDir, filename);
+  await Jimp.read(tmpUpload).then((imageProcessing) => {
+    imageProcessing.resize(250, 250).writeAsync(tmpUpload);
+  });
 
-    Jimp.read(tempUpload, (err, img) => {
-        if (err) throw err;
-        img.resize(250, 250).write(resultUpload);
-    });
-    await fs.rename(tempUpload, resultUpload);
-    const avatarURL = path.join("avatars", filename);
-
-    await User.findByIdAndUpdate(_id, { avatarURL });
-
+  const { _id: id } = req.user;
+  const imageName = `${id}_${originalname}`;
+  try {
+    const resultUpload = path.join(avatarDir, imageName);
+    await fs.rename(tmpUpload, resultUpload);
+    const avatarURL = path.join("avatars", imageName);
+    await User.findByIdAndUpdate(req.user._id, { avatarURL });
     res.json({ avatarURL });
- };
+  } catch (error) {
+    await fs.unlink(tmpUpload);
+    throw error;
+  }
+};
+
+
+
+
+// Як в лекції
+// const avatarPath = path.resolve("public", "avatars");
+ 
+// const updateAvatar = async (req, res) => {
+//     const { _id } = req.user;
+//     const { path: oldPath, filename } = req.file;
+//     const newPath = path.join(avatarPath, filename);
+
+//     await fs.rename(oldPath, newPath);
+//     const avatar = path.join("public", "avatars", filename);
+//     const result = await User.create({ ...req.body, avatar, _id });
+
+//     res.status(201).json(result);
+// };
+ 
+
 
 // Зберігаємо файли в хмарі
 //  const updateAvatar = async (req, res) => {
